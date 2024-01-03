2f5fd20875721f652f368dcdbe63aa5056fa24af
iovsomnium <ksw09157.5212@gmail.com>
2024-01-04 00:41:43 +0900

    fix: edit wrong path, wrong save function

--- batch/scraper_batch.py
+++ batch/scraper_batch.py
@@ -1,11 +1,28 @@
-#run batch server
-
+import os
+import sys
+from dotenv import load_dotenv
+from env.conf import News
 from scraper.scraper import NewsScraper
+from db_handler.db_handler import NewsTable
+from typing import List
 
-search_company = "삼성전자"
+load_dotenv('env/data.env')
+sys.path.insert(0, os.getenv('device_path'))
 
-news_scrapper = NewsScraper()
-result = news_scrapper.crawl_news(search_company, 1, 400)
-with open("news_urls.txt", "a", encoding="utf-8") as file:
-        file.write(str(result)+"\n")
+def crawl_news(search: str, start_pg: int, end_pg: int) -> List[News]:
+    scraper = NewsScraper()
+    urls = scraper.makeUrl(search, start_pg, end_pg)
+    final_urls = scraper.process_urls(urls)
 
+    return [scraper.process_each_url(url) for url in final_urls]
+
+def save_news_to_db(company: str, page_start: int, page_end: int):
+    news_list = crawl_news(company, page_start, page_end)
+    
+    news_table = NewsTable()
+
+    for news in news_list:
+        news_table.insert(news)
+
+
+save_news_to_db("삼성전자", 1, 400)
